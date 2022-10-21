f0f33fc8061ecf285b6c9d9622a8f2f6394e96f9
andrearanica <andrearanica2004@gmail.com>
2022-10-21 17:28:04 +0200

    🚀 Things added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,9 @@ import '../App.css'
 
 function Navbar () {
     return (
-        <nav class="navbar navbar-dark" style={{ backgroundColor: "#1746A2" }}>
-        <a class="navbar-brand" href="#">
-            <img style={{ borderRadius: "8px", maginLeft: "8px", marginRight: "8px" }} src="https://avatars.githubusercontent.com/u/115826499?s=200&v=4" width="30" height="30" class="d-inline-block align-top" alt="" />
+        <nav class="navbar navbar-dark" style={{ backgroundColor: "#06283D" }}>
+        <a class="navbar-brand" href="App.js" style={{ paddingLeft: "15px" }}>
+            <img style={{ borderRadius: "8px", marginRight: "15px" }} src="https://avatars.githubusercontent.com/u/115826499?s=200&v=4" width="30" height="30" class="d-inline-block align-top" alt="" />
             Valtrighe Eagles
         </a>
         </nav>
